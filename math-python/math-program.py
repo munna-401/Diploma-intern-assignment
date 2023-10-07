0d0eaacb2a2cf69102faebc9e8dbcadf8f467959
Moniruzzaman <mzmunna401@gmail.com>
2023-10-08 02:45:00 +0600

    add file

---
+++ math-python/math-program.py
@@ -0,0 +1,37 @@
+#summation
+def sum_4_number(first, second, ):
+    print(f"Sum of {first} + {second} = {first + second}")
+
+
+sum_4_number(10, 20, )
+
+
+
+#subtraction
+
+def subtraction_4_number(first, second, ):
+    print(f"Your Subtraction Result is {first} - {second} = {first - second}")
+
+
+subtraction_4_number( 40, 20)
+
+#multiplication
+
+def multiplication_4_number(first, second, third, fourth):
+    return first * second * third * fourth
+
+
+multiply= multiplication_4_number(30, 10, 20, 40)
+print(f"multiplication result is : {multiply}")
+
+#division
+def div_2_number(first, second):
+    return first / second
+
+
+devide = div_2_number(50, 2)
+print(f"Your Division result is : {devide}")
+
+
+
+
---
+++ method-python/method-python.py
@@ -0,0 +1,42 @@
+#Python Method Calling
+
+def my_name_is():
+    print("My name is Md Moniruzzaman Munna")
+
+
+def i_have_enrolled_course(course_name):
+    print(f"I have enrolled in a course named {course_name}")
+
+
+def i_have_learning(backend, frontend):
+    return f"Learning {backend}, {frontend}"
+
+course_and_learn=[{
+   " course": "Python & Web",
+   " learn": "Python, HTML, CSS, Bootstrap"
+   },
+   {
+        "course": "Java Spring Boot",
+    "learn ":" Java, HTML, CSS, Hibernet"
+    },
+    {
+        "course ": "C# & ASP.NET Core",
+     "learn": " C#, Entity Framework, Razor"
+    },
+    {
+        "course": "MERN Development",
+     "learn": "Node, React, HTML, CSS"
+     },
+     { 
+         "course": "PHP & Laravel",
+        "learn": "PHP, Blade, Eloquent"
+```}
+]
+
+
+for course in course_and_learn:
+    my_name_is()
+    i_have_enrolled_course(course)
+    result=i_have_learning(course)
+    print(result)
+    print()
